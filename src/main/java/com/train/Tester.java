82e56913b492a4403e5cb7b9c24f65833cf4e583
rl0217 <rlee@mail2000.com.tw>
2019-09-27 02:23:49 +0800

    1.2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
 
-      Scanner scan=new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);
+        int countAll=0;
 
-        System.out.println("Please enter number of tickets:");
-        int countAll = scan.nextInt();
+        while (countAll !=-1) {
+            System.out.println("Please enter number of tickets:");
+            countAll = scan.nextInt();
+            if (countAll == -1) {
+                break;
+            };
 
-        System.out.println("How many round-trip tickets:");
-        int countR=scan.nextInt();
+            System.out.println("How many round-trip tickets:");
+            int countR = scan.nextInt();
 
-        Ticket T = new Ticket(1000,2000,countAll,countR);
+            Ticket T = new Ticket(1000, 2000, countAll, countR);
 
-        T.TotalPrice();
-        T.print();
-
+            T.TotalPrice();
+            T.print();
+        }
+//            System.out.println("Do you want to buy more tickets?");
+//            int answer=scan.nextInt();
 
     }
 }
